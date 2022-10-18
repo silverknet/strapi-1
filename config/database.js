1863ad5447546e2f57d782731526080319d8a654
marhaka <marhaka@kth.se>
2022-10-18 21:08:25 +0200

    local app

--- config/database.js
+++ config/database.js
@@ -1,8 +1,23 @@
+/*module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('PGHOST', 'localhost'),
+      port: env.int('PGPORT', 5432),
+      database: env('PGDATABASE', 'postgres'),
+      user: env('PGUSER', 'postgres'),
+      password: env('PGPASSWORD', 'Nisseha1997'),
+      ssl: env.bool(false),
+    },
+  },
+});
+*/
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
+      host: env('PGHOST', 'localhost'),
       port: env.int('PGPORT', 5432),
       database: env('PGDATABASE', 'strapi'),
       user: env('PGUSER', 'strapi'),
