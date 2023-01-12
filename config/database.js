a8b5f6882e3d46a33860642c6f7a290c19b62c95
marhaka <marhaka@kth.se>
2023-01-12 15:00:00 +0100

    a1

--- config/database.js
+++ config/database.js
@@ -20,7 +20,7 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
+      port: env.int('PGPORT', 7020),
       database: env('PGDATABASE', 'strapi'),
       user: env('PGUSER', 'strapi'),
       password: env('PGPASSWORD', 'password'),
